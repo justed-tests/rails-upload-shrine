c34f88975c74988f5f02351804f7b85852fec0ec
justed <dev.justed@gmail.com>
2017-02-26 17:12:23 +0500

    more plugins

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 # !?
 class PhotosController < ApplicationController
+  before_action :set_photo, only: [:edit, :update]
   def index
     @photos = Photo.all
   end
@@ -19,9 +20,25 @@ class PhotosController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @photo.update(photo_params)
+      flash[:success] = 'Photo updated!'
+      redirect_to photos_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def photo_params
-    params.require(:photo).permit(:title, :image)
+    params.require(:photo).permit(:title, :image, :remove_image)
+  end
+
+  def set_photo
+    @photo = Photo.find(params[:id])
   end
 end
--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -1,5 +1,9 @@
 # magic inhabits this place
 class ImageUploader < Shrine
+  plugin :determine_mime_type
+  plugin :cached_attachment_data
+  plugin :remove_attachment
+
   Attacher.validate do
     validate_max_size 1.megabyte, message: 'file is too large'
     validate_mime_type_inclusion ['image/jpg', 'image/jpeg', 'image/png'],
